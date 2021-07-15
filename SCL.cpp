58b99a2dbf7287998709f53850946d2200d2fb7d
trigger337 <72268042+trigger337@users.noreply.github.com>
2021-07-15 20:38:15 +0300

    Add files via upload

---
+++ SCL.cpp
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<windows.h>
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+float version = 1.0;
+string cmds[] = {"y", "n", "help", "exit", "cl"};
+
+void init()
+{
+	cout << "simple command line version " << version << endl << "use 'help' to list all available commands" << endl;
+}
+
+void help()
+{
+	cout << "available commands" << endl;
+	for(int i = 2; i < 5; i++)
+		cout << cmds[i] << endl;
+}
+
+int main()
+{	
+	init();
+	
+	string inp;
+	
+	while(1)
+	{
+		cout << "TCL > ";
+		getline(cin, inp);
+		
+		if(inp == cmds[2])
+			help();
+		if(inp == cmds[3])
+			exit(0);
+		if(inp == cmds[4])
+			system("cls");
+		
+	}
+}
